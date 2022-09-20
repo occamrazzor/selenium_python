f617851885c44cc45e8d47b2563f1f63901a3f80
occam_razor <occam_razor@hotmail.com>
2022-09-20 01:37:38 -0700

    Object Oriented Programming

---
+++ oop_fun.py
@@ -0,0 +1,29 @@
+# methods , instance variables, class variables, constructor 
+# Class name start with Capital Case.
+class Calculator:
+    num = 100   #Class variable accessible withn any method in the Class and constant across object
+                # and needs to be called within the class using class name eg. Calculator.num or self.num
+
+
+    def __init__(self, a, b): # Constructor is called first and the name is __init__
+        self.firstNumber = a   # instance variable differs for every object 
+        self.secondNumber = b  # instance variable differs for every object
+        print("I am called automatically when object is created")
+
+    def getData(self): # Method
+        print('I am now executing as method in class')
+
+    def Summation(self):
+        return self.firstNumber + self.secondNumber  + Calculator.num 
+
+
+
+
+obj1 = Calculator(3, 4) #syntax to create an object from the class
+obj1.getData()
+print(obj1.Summation())
+
+
+obj2 = Calculator(4, 6) #syntax to create an object from the class
+obj2.getData()
+print(obj2.Summation())

8badc5a758d8d44f89a2853a3696e7dba6198b02
occam_razor <occam_razor@hotmail.com>
2022-10-02 16:34:46 -0700

    Loop thru table items and select

--- pythonSelenium/Assingment_Functional.py
+++ pythonSelenium/Assingment_Functional.py
@@ -1,4 +1,4 @@
-
+from gettext import find
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -6,7 +6,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
-# Expected list ['Cucumber - 1kg', 'Raspberry' - 1/4 kg', 'Strawberry - 1/4 kg']
+expectedList = ['Cucumber - 1 Kg', 'Raspberry - 1/4 Kg', 'Strawberry - 1/4 Kg']
+actualList = []
 
 options = Options()
 options.add_argument("--disable-web-security")
@@ -14,15 +15,23 @@ options.add_argument("--disable-web-security")
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 service_obj = Service("c:/Webdrivers/chromedriver.exe")
 driver = webdriver.Chrome(service=service_obj, options=options)
-driver.implicitly_wait(5) # This timeout is applied globally and wait will be over the moment the element appear.
+driver.implicitly_wait(2) # This timeout is applied globally and wait will be over the moment the element appear.
 driver.get("https://www.rahulshettyacademy.com/seleniumPractise/#/")
 driver.find_element(By.CSS_SELECTOR,'.search-keyword').send_keys("ber")
-#driver.find_element(By.XPATH, "//input[@type='search']").send_keys('ber')
-time.sleep(2) # This sleep is required for a list see below.
+time.sleep(4)
+
 results = driver.find_elements(By.XPATH, "//div[@class='products']/div") # This generates a list of elements
 count = len(results)
 print(count)
 assert count > 0 
+for result in results: #chaining the products  ie chaining parent to child elements...Returns a list[]
+    actualList.append(result.find_element(By.CSS_SELECTOR, "h4.product-name").text)
+    #result.find_element(By.XPATH, "div/button").click()
+print(actualList)
+print(expectedList)
+assert expectedList == actualList
+
+#############################################
 for result in results: #chaining the products to the cart ie chaining parent to child elements...Returns a list[]
     result.find_element(By.XPATH, 'div/button').click()
 
@@ -48,3 +57,7 @@ driver.find_element(By.CSS_SELECTOR, ".promoBtn").click()
 wait = WebDriverWait(driver, 10)
 wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, ".promoInfo")))
 print(driver.find_element(By.CLASS_NAME, 'promoInfo').text)
+
+discountedAmount = float(driver.find_element(By.CSS_SELECTOR,".discountAmt").text)
+print (float(discountedAmount))
+assert totalAmount > discountedAmount

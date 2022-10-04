8dd6cb44d55437299803d354ca60c5b4548f731c
occam_razor <occam_razor@hotmail.com>
2022-10-03 19:31:03 -0700

    ActionChain added

--- pythonSelenium/actionsHover.py
+++ pythonSelenium/actionsHover.py
@@ -1,5 +1,6 @@
 import time
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -13,3 +14,10 @@ options.add_argument("--disable-web-security")
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 service_obj = Service("c:/Webdrivers/chromedriver.exe")
 driver = webdriver.Chrome(service=service_obj, options=options)
+
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+action = ActionChains(driver)
+action.double_click(driver.find_element(By.))
+action.context_click()
+action.drag_and_drop()
+action.move_to_element()

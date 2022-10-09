9a4f586c042ed0e9c06a1fa789b8e0d1561ecfca
occam_razor <occam_razor@hotmail.com>
2022-10-09 02:08:36 -0700

    Created BaseClass.py for logging

---
+++ pytestList/BaseClass.py
@@ -0,0 +1,14 @@
+import inspect
+import logging
+
+
+class BaseClass:
+    def getLogger(self):
+        loggerName = inspect.stack()[1][3]
+        logger = logging.getLogger(loggerName)
+        fileHandler = logging.FileHandler('logfile.log')
+        formatter = logging.Formatter("%(asctime)s :%(levelname)s : %(name)s :%(message)s")
+        fileHandler.setFormatter(formatter)
+        logger.addHandler(fileHandler)  # filehandler object determines what format and file to print in
+        logger.setLevel(logging.DEBUG)  # note this can be set as INFO or DEBUG
+        return logger
--- pytestList/test_fixturesData.py
+++ pytestList/test_fixturesData.py
@@ -1,12 +1,18 @@
+
 import pytest
+
+from pytestList.BaseClass import BaseClass
 
 
 # Note: dataLoad is passed to def editProfile as parameter because it is returning data see conftest.py
+# Note: class TestExample2(is passed the parent class...BaseClass) and the import statement above.
+
 
 @pytest.mark.usefixtures("data_load")
-class TestExample2:
+class TestExample2(BaseClass):
 
     def test_editProfile(self, data_load):
-        print(data_load)
-        print(data_load[0])
+        log = self.getLogger()
+        log.info(data_load[0])
+        log.info(data_load[2])
         print(data_load[2])
